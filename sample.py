2e82886b6c6d2722466d827cf37019218e4c4491
exam <jwarkhade9@gmail.com>
2023-08-23 14:03:29 +0530

    first commit

---
+++ sample.py
@@ -0,0 +1,14 @@
+n=4
+for i in range(n+1):
+    for j in range(i):
+        print('*',end='')
+    print(" ")
+
+    """echo "# NLP_Labb" >> README.md
+git init
+git add README.md
+git commit -m "first commit"
+git branch -M main
+git remote add origin https://github.com/Coding-with-Jayavant/NLP_Labb.git
+git push -u origin main
+    """
